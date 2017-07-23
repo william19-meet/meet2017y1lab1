3efa39a74fe798d738d6f2466b0ac683ea8635ad
william19-meet <william19@meet.mit.edu>
2017-07-23 22:20:27 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,88 @@
+import turtle
+
+#everything that comes after the # is a comment.
+#it is a note to the person reading the code.
+#the computer ignored it.
+#write your code bekiw here...
+
+
+
+#...and end it before the next line.
+turtle.penup()
+
+turtle.goto(-200,-100)
+
+turtle.pendown()
+
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+
+turtle.penup()
+
+turtle.goto(-75,-100)
+turtle.pendown()
+turtle.goto(-75,-100+200)
+
+
+turtle.penup()
+turtle.goto(-75,-100)
+turtle.pendown()
+turtle.goto(-75+100,-100)
+
+turtle.penup()
+
+turtle.goto(-75,-100+100)
+turtle.pendown()
+turtle.goto(-75+100,-100+100)
+
+
+turtle.penup()
+
+turtle.goto(-75,-100+200)
+turtle.pendown()
+turtle.goto(-75+100,-100+200)
+
+#2nd E down
+turtle.penup()
+
+turtle.goto(-75+120,-100)
+turtle.pendown()
+turtle.goto(-75+120,-100+200)
+
+
+turtle.penup()
+turtle.goto(-75+120,-100)
+turtle.pendown()
+turtle.goto(-75+220,-100)
+
+turtle.penup()
+
+turtle.goto(-75+120,-100+100)
+turtle.pendown()
+turtle.goto(-75+220,-100+100)
+
+
+turtle.penup()
+
+turtle.goto(-75+120,-100+200)
+turtle.pendown()
+turtle.goto(-75+220,-100+200)
+
+#T in meet
+turtle.penup()
+turtle.goto(-75+340,-100)
+turtle.pendown()
+turtle.goto(-75+340,-100+200)
+
+
+turtle.penup()
+turtle.goto(-75+340,-100+200)
+turtle.pendown()
+turtle.goto(-75+240,-100+200)
+turtle.goto(-75+440,-100+200)
+
+turtle.mainloop()
+
